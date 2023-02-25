c88f2b7d494b3ec716bc3ccf50d8aac467585bdb
Sanesh Gobin <saneshgobin@icloud.com>
2023-02-25 19:04:45 +0200

    Change text color to pink

--- testRepo/ViewController.swift
+++ testRepo/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         label.backgroundColor = .black
+        label.textColor = .purple
     }
 
 
